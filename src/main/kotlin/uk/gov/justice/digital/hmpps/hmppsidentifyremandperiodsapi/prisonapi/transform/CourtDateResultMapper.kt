55c49d5b9ca26dfe3ae46a95638de5536c7b3aa8
ldlharper <luke.harper@digital.justice.gov.uk>
2023-08-24 14:36:15 +0100

    ADJUST1-150 remove booking no from message. (#27)
    * ADJUST1-150 remove booking no from message.
    
    * ADJUST1-152 Allowing fronend to decide to include booking ID or not.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsidentifyremandperiodsapi/prisonapi/transform/CourtDateResultMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsidentifyremandperiodsapi/prisonapi/transform/CourtDateResultMapper.kt
@@ -182,7 +182,7 @@ fun mapCourtDateResult(courtDateResult: PrisonApiCourtDateResult, issuesWithLega
     "FPR" -> STOP
 
     else -> {
-      issuesWithLegacyData.add(LegacyDataProblem("The court event on ${courtDateResult.date.format(DateTimeFormatter.ofPattern("d MMM yyyy"))} for offence ${courtDateResult.charge.offenceDescription} committed at ${courtDateResult.charge.offenceDate!!.format(DateTimeFormatter.ofPattern("d MMM yyyy"))} within booking ${courtDateResult.bookNumber} has an unsupported outcome ${courtDateResult.resultCode}: ${courtDateResult.resultDescription}", courtDateResult))
+      issuesWithLegacyData.add(LegacyDataProblem("The court event on ${courtDateResult.date.format(DateTimeFormatter.ofPattern("d MMM yyyy"))} for offence ${courtDateResult.charge.offenceDescription} committed at ${courtDateResult.charge.offenceDate!!.format(DateTimeFormatter.ofPattern("d MMM yyyy"))} has an unsupported outcome ${courtDateResult.resultCode}: ${courtDateResult.resultDescription}", courtDateResult))
       null
     }
   }
