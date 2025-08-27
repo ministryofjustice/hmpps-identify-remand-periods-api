7fafd7a2297fb11835b92ad688d39d6f6a4207f1
James Baker <james.baker3@digital.justice.gov.uk>
2025-08-27 10:48:09 +0100

    ADJST-996: Fix locale of date format
    We were getting inconsistent results from github actions and local running for September which is "Sept" for UK and "Sep" in US which is what we seem to get for CI.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsidentifyremandperiodsapi/util/LocalDateExt.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsidentifyremandperiodsapi/util/LocalDateExt.kt
@@ -2,11 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsidentifyremandperiodsapi.util
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 fun LocalDate.isBeforeOrEqualTo(date: LocalDate): Boolean = this.isBefore(date) || this == date
 
 fun LocalDate.isAfterOrEqualTo(date: LocalDate): Boolean = this.isAfter(date) || this == date
 
 fun LocalDate.mojDisplayFormat(): String = this.format(
-  DateTimeFormatter.ofPattern("d MMM yyyy"),
+  DateTimeFormatter.ofPattern("d MMM yyyy", Locale.UK),
 )
