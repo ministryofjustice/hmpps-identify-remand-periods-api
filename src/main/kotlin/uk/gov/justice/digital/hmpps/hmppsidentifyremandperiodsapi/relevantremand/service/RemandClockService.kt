c41ba5580fd91ad9aaf31f66abde691c371f3361
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2025-09-17 16:34:19 +0100

    Linting fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsidentifyremandperiodsapi/relevantremand/service/RemandClockService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsidentifyremandperiodsapi/relevantremand/service/RemandClockService.kt
@@ -24,7 +24,7 @@ class RemandClockService {
             from = it
           }
           if (it.type == STOP && from != null) {
-            remand.add(ChargeRemand(from!!.date, getToDate(it), CourtAppearance(from!!.date, from!!.description), CourtAppearance(it.date, it.description), listOf(chargeAndEvent.charge.chargeId), null,))
+            remand.add(ChargeRemand(from!!.date, getToDate(it), CourtAppearance(from!!.date, from!!.description), CourtAppearance(it.date, it.description), listOf(chargeAndEvent.charge.chargeId), null))
             from = null
           }
         }
