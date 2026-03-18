acc27d3ff537de7b89587a9d72333cd473c7dc4f
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2026-03-18 13:44:07 +0000

    Add missed dependency to fix the int tests failures

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-flyway")
 
   // Database dependencies
   runtimeOnly("org.flywaydb:flyway-core")
